c0fa90d7869c8a6ff9b475c44b623c7e1aeb2061
guliyevashahjahan <shahjahaneg@code.edu.az>
2023-08-15 21:53:55 +0400

    MVC AppBasics

---
+++ BigOn BackEnd/BigOn WebUI/Program.cs
@@ -0,0 +1,23 @@
+namespace BigOn_WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+                });
+
+            app.Run();
+        }
+    }
+}
