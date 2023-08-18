6aa48b54759da79583c1aae30453299a17bd9316
guliyevashahjahan <shahjahaneg@code.edu.az>
2023-08-18 16:37:06 +0400

    merge

---
+++ BigOn BackEnd/BigOn WebUI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigOn_WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Subscribe(string email)
+        {
+            return Json(new
+            {
+                error=false,
+                email
+            });
+        }
+        public IActionResult SubscribeApprove()
+        {
+            return Content("You've successfully subscribed!");
+        }
+    }
+
+}
